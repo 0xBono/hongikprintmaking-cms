44c84809b86651484c85b682ffed29d21e1c042b
0xbono <iam@sonjim.in>
2021-11-11 07:31:22 +0900

    chore(init): 프레임워크 보일러플레이트 구성

---
+++ src/pages/index.tsx
@@ -0,0 +1,13 @@
+import { GNBLayout } from 'components/Layouts';
+
+const Index = () => {
+  return (
+    <GNBLayout>
+      <div style={{ backgroundColor: '#fff', height: '100%' }}>
+        <h1>Hello World</h1>
+      </div>
+    </GNBLayout>
+  );
+};
+
+export default Index;
