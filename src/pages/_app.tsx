adf7f22daf64c663d622f9709cb308e192e73805
0xbono <iam@sonjim.in>
2021-11-11 08:15:01 +0900

    feat(pages/_app): Chakra-ui 사용을 위해 상위에 ChakraProvider 추가

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,12 @@
 import { AppProps } from 'next/app';
+import { ChakraProvider } from '@chakra-ui/react';
 import 'styles/global.scss';
 
 function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <ChakraProvider>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  );
 }
 export default App;
