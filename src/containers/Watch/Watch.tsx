9952216a2bd6f5d5144f18a392e62ab1c10133dd
0xbono <iam@sonjim.in>
2021-11-11 14:25:13 +0900

    feat(containers/watch): 졸업전시회 작품 관리 페이지 레이아웃 추가

---
+++ src/containers/Watch/Watch.tsx
@@ -0,0 +1,87 @@
+import { PageHeader, Table, Tag, Space } from 'antd';
+
+export const Watch = () => {
+  const columns = [
+    {
+      title: 'Name',
+      dataIndex: 'name',
+      key: 'name',
+      render: (text) => <a>{text}</a>,
+    },
+    {
+      title: 'Age',
+      dataIndex: 'age',
+      key: 'age',
+    },
+    {
+      title: 'Address',
+      dataIndex: 'address',
+      key: 'address',
+    },
+    {
+      title: 'Tags',
+      key: 'tags',
+      dataIndex: 'tags',
+      render: (tags) => (
+        <>
+          {tags.map((tag) => {
+            let color = tag.length > 5 ? 'geekblue' : 'green';
+            if (tag === 'loser') {
+              color = 'volcano';
+            }
+            return (
+              <Tag color={color} key={tag}>
+                {tag.toUpperCase()}
+              </Tag>
+            );
+          })}
+        </>
+      ),
+    },
+    {
+      title: 'Action',
+      key: 'action',
+      render: (text, record) => (
+        <Space size="middle">
+          <a>수정하기</a>
+          <a>삭제하기</a>
+        </Space>
+      ),
+    },
+  ];
+
+  const data = [
+    {
+      key: '1',
+      name: 'John Brown',
+      age: 32,
+      address: 'New York No. 1 Lake Park',
+      tags: ['nice', 'developer'],
+    },
+    {
+      key: '2',
+      name: 'Jim Green',
+      age: 42,
+      address: 'London No. 1 Lake Park',
+      tags: ['loser'],
+    },
+    {
+      key: '3',
+      name: 'Joe Black',
+      age: 32,
+      address: 'Sidney No. 1 Lake Park',
+      tags: ['cool', 'teacher'],
+    },
+  ];
+
+  return (
+    <PageHeader
+      className="site-page-header"
+      onBack={() => null}
+      title="졸업전시회 작품 관리"
+      subTitle="등록된 작품을 관리할 수 있는 페이지입니다."
+    >
+      <Table columns={columns} dataSource={data} />
+    </PageHeader>
+  );
+};
