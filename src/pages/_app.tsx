fdf3ee6562786f5dffc98b87c639dc34b627dc41
0xbono <iam@sonjim.in>
2021-11-11 09:33:20 +0900

    chore(deps): chakra-ui 제거 및 ant-design 라이브러리 적용

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,7 @@
 import { AppProps } from 'next/app';
-import { ChakraProvider } from '@chakra-ui/react';
 import 'styles/global.scss';
 
 function App({ Component, pageProps }: AppProps) {
-  return (
-    <ChakraProvider>
-      <Component {...pageProps} />
-    </ChakraProvider>
-  );
+  return <Component {...pageProps} />;
 }
 export default App;
